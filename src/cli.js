0de15893149ac80c059e1e2429a180cf9a72d97f
Bruno Mota <bruno7motadev@gmail.com>
2024-08-13 21:13:58 -0300

    add commander and chalk in project

--- src/cli.js
+++ src/cli.js
@@ -1,46 +1,58 @@
 //CLI - Command Line Interface (Interface de Linha de Comando)
 import fs from 'fs';  // File system
+import path from 'path'; // Path
 import { contarPalavras } from './index.js';
 import trataErros from './erros/funcoesErro.js';
 import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
 
-const caminhoArquivo = process.argv; //vetor de argumento
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try {
-        if (erro) throw erro
-        const resultado = contarPalavras(texto);
-        criaESalvaArquivo(resultado, endereco)
-    } catch (erro) {
-        trataErros(erro)
-    }
-}) 
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+    .action((options) => {
+        const { texto, destino } = options;
 
+        if (!texto || !destino) {
+            console.error('erro: insira o caminho de origem e destino');
+            program.help();
+            return;
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino);
+            console.log('texto processado com sucesso!');
+        } catch (erro) {
+            console.log('ocorreu um erro no processamento: ', erro);
+        }
+    })
+
+program.parse()
+
+function processaArquivo(texto, destino) {
+    fs.readFile(texto, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro;
+            const resultado = contarPalavras(texto);
+            criaESalvaArquivo(resultado, destino);
+        } catch (erro) {
+            trataErros(erro);
+        }
+    }) 
+}
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`
     const textoPalavras = montaSaidaArquivo(listaPalavras)
     try {
         await fs.promises.writeFile(arquivoNovo, textoPalavras);
-        console.log(`Arquivo criado com sucesso!`)
+        console.log(`Arquivo criado com sucesso`)
     } catch (erro) {
         throw erro
     }
 } 
-
-/* 
-function criaESalvaArquivo(listaPalavras, endereco) {
-    const arquivoNovo = `${endereco}/resultado.txt`
-    const textoPalavras = JSON.stringify(listaPalavras)
-    fs.promises.writeFile(arquivoNovo, textoPalavras)
-        .then(() => {
-            console.log(`Arquivo criado com sucesso!`)
-        })
-        .catch((erro) => {
-            throw erro
-        })
-        .finally(() => console.log('Operação finalizada.'))
-}
- */
